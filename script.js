dc7c4103d8b5456a4230d498080c4141edfc8a89
aditya kapile <you@example.com>
2023-10-11 23:19:07 +0530

    major2

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+gallary;
+
+function loadMore() {
+  let gallary = document.querySelector(".gallary");
+  let load_btn = document.querySelector(".load-more");
+
+  gallary.style.height = "140vh";
+}
+
+// slideshow
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
