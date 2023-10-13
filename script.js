fe58722439d6f3db842dbd8f04d3b6e467a8a9e4
aditya kapile <you@example.com>
2023-10-14 01:42:54 +0530

    major changes making

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+const theBody = document.querySelector("body");
+const openNav = document.querySelector(".menu-bar button");
+const closeNav = document.querySelector(".close-nav button");
+const Navbar = document.querySelector(".navbar");
+
+// Hiding body scroll when mobile navigation menu opens
+function bodyScroll() {
+  if (Navbar.classList.contains("show")) {
+    theBody.classList.add("hide-scroll");
+  } else if (theBody.classList.contains("hide-scroll")) {
+    theBody.classList.remove("hide-scroll");
   }
 }
+
+function showHide() {
+  Navbar.classList.toggle("show");
+  bodyScroll();
+}
+
+openNav.onclick = showHide;
+closeNav.onclick = showHide;
 
 const obj = document.querySelector(".objectives");
 const mis = document.querySelector(".mission");
